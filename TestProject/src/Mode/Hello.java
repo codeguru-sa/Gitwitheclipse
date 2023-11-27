0f4f2db1d7fac5921092fabaf14b7b4f1c5f94b1
codeguru-sa <guptagurucharan4199@gmail.com>
2023-11-27 16:42:41 +0530

    Implemented Stream

--- TestProject/src/Mode/Hello.java
+++ TestProject/src/Mode/Hello.java
@@ -1,7 +1,24 @@
 package Mode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello github");
+		List<Integer> list = new ArrayList<Integer>();
+		
+		list.add(12);
+		list.add(18);
+		
+		list.add(23);
+		list.add(15);
+		
+		list
+		.stream()
+		.filter(number -> number%2==0)
+		.forEach(System.out::println);
+		
+	
+		
 	}
 }
